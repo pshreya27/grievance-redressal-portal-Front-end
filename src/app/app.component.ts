17784ead7634351a4078cbde3d8bbbd0d51baa7e
arjun3649 <arjun.rakwal3649@gmail.com>
2024-07-18 12:10:21 +0530

    Home Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GroupBoxComponent } from "./group-box/group-box.component";
 import { CarouselComponent } from "./carousel/carousel.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { NgClass ,NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GroupBoxComponent, CarouselComponent, DashboardComponent],
+  imports: [RouterOutlet, GroupBoxComponent, CarouselComponent, DashboardComponent,NgClass,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'grievance-redressal-portal';
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log('Menu toggled:', this.isMenuOpen); // For debugging
+  }
 }
